844d955f76672043c2ed656a5092f54a4c11c71d
lukaskwkw <lukaskwkw@gmail.com>
2015-11-27 10:02:13 +0100

    new project for fcc

---
+++ public/assets/js/main.js
@@ -0,0 +1,9 @@
+// https://en.wikipedia.org/w/api.php?action=opensearch&search=Katow
+//TO-DO
+/*
+
+[DONE]1.Check how to recive response 
+list of queried complete request
+-- just use above link
+
+*/
