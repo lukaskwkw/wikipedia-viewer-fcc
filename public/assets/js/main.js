19983e792dee2b82f675651b29ff85e179a72680
lukaskwkw <lukaskwkw@gmail.com>
2015-12-01 17:23:21 +0100

    final product ready to put on fcc

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,98 +1,107 @@
-// 
-//TO-DO
-/*
+$(document).ready(function() {
 
-[DONE]1.Check how to recive response 
-list of queried complete request
--- just use above link
+	var body = $("body");
+	var searchBox = $(".search-box");
+	var searchBar = $(".search-bar");
+	var view = $(".view");
+	var viewList = $(".view__list");
+	var prevLength = searchBar.val().length;
+	var viewHeight = view.height();
+	var info = $(".info");
+	var clearBtn = $(".clear-btn");
+	var ranBtn = $(".ran-btn");
 
-*/
-
-$(document).ready(function (){
-
-// var url = "//en.wikipedia.org/w/api.php?action=opensearch&search=";
-var body =$("body");
-var searchBox = $(".search-box");
-var searchBar = $(".search-bar");
-var view=$(".view");
-var viewList=$(".view__list");
-var prevLength = searchBar.val().length;
-var viewHeight = view.height();
-var info = $(".info");
-var clearBtn = $(".clear-btn");
-
-clearBtn.on("click", function(){
-	searchBar.val("");
-	view.stop();
-	view.slideUp(500,function(){
+	clearBtn.on("click", function() {
+		searchBar.val("");
+		view.stop();
+		view.slideUp(500, function() {
 			info.show();
-	});
-	searchBox.css("transform","translate(0,0)");
-	$(this).hide(200);
-})
+		});
+		searchBox.css("transform", "translate(0,0)");
+		$(this).hide(200);
+		searchBar.focus();
+	})
 
-searchBar.keyup(function() {
-	var currentLength = $(this).val().length;
-		if (currentLength<1)
-		{
+	ranBtn.on('click', randomSearch);
+
+	searchBar.keyup(function() {
+		var currentLength = $(this).val().length;
+		if (currentLength < 1) {
 			view.stop();
-			view.slideUp(500,function  () {
-						info.show();
+			view.slideUp(500, function() {
+				info.show();
 			});
-		searchBox.css("transform","translate(0,0)");
+			searchBox.css("transform", "translate(0,0)");
 
-		clearBtn.hide();
-		}
-		else if (currentLength!==prevLength)
-		{		
+			clearBtn.hide();
+		} else if (currentLength !== prevLength) {
 			query()
 			prevLength = currentLength;
-		clearBtn.show(500);
+			clearBtn.show(500);
 
 		}
 	});
 
-function query() {
-	$.ajax({
-      url: 'http://en.wikipedia.org/w/api.php',
-      data: { action: 'opensearch', search: searchBar.val(), format: 'json' },
-      dataType: 'jsonp',
-      success: function(json){
-      	// console.log(json);
-      	// console.log(viewHeight);
-		searchBox.css("transform","translate(0,"+(viewHeight/2)+"px)");
-		info.hide();
-		// searchBar.css({position:"relative",
-		// 	margin: "1rem auto"});
-		viewList.html("");
-		var dataDescriptions = json[2];
-		var dataUrls = json[3];
-		for (var i = 0; i < dataDescriptions.length; i++) {
-			if (dataDescriptions[i]=="")
-			viewList.append('<a href="'+dataUrls[i]+'" target="_blank"><li class="list-item">'+dataUrls[i]+"</li></a>")
-				else
-			viewList.append('<a href="'+dataUrls[i]+'" target="_blank"><li class="list-item">'+dataDescriptions[i]+"</li></a>")
-		};
-		if (searchBar.val().length<1){
+	function randomSearch () {
+		$.ajax({
+			url: 'http://en.wikipedia.org/w/api.php',
+			dataType: 'jsonp',
+			data: {action: 'query', list: 'random', rnlimit: '10', rnnamespace: 0, format: 'json'},
+			success: function (json) {
+				console.log(json);
+				searchBox.css("transform", "translate(0," + (viewHeight / 2) + "px)");
+				info.hide();
+				viewList.html("");
+				clearBtn.show(500);
+				var randomList = json.query.random;
+				for (var i = 0; i < randomList.length; i++) {
+					viewList.append('<a href="https://en.wikipedia.org/wiki/' + randomList[i].title + '" target="_blank"><li class="list-item"><span class="text--special">' + randomList[i].title + '</span></li></a>');				
+				};
+				view.stop();
+				view.slideDown(100, function() {
+					body.scrollTop(searchBox.offset().top);
+				})
+				searchBar.focus();
+			}
+		});
+
+	}
+
+	function query() {
+		$.ajax({
+			url: 'http://en.wikipedia.org/w/api.php',
+			data: {
+				action: 'query',
+				list: "search",
+				srsearch: searchBar.val(),
+				format: 'json'
+			},
+			dataType: 'jsonp',
+			success: function(json) {
+				searchBox.css("transform", "translate(0," + (viewHeight / 2) + "px)");
+				info.hide();
+				viewList.html("");
+				var result = json.query.search;
+				for (var i = 0; i < result.length; i++) {
+					viewList.append('<a href="https://en.wikipedia.org/wiki/' + result[i].title + '" target="_blank"><li class="list-item"><span class="text--special">' + result[i].title + '</span><p>' + result[i].snippet + '</p></li></a>');
+				};
+				if (searchBar.val().length < 1) {
 					view.stop();
-					view.slideUp(500,function(){
+					view.slideUp(500, function() {
 						info.show();
 					});
-		searchBox.css("transform","translate(0,0)");
-		
+					searchBox.css("transform", "translate(0,0)");
 
 					return;
 				}
 
-		view.stop();
-		view.slideDown(100,function() {
-			body.scrollTop(searchBox.offset().top);
-		})
+				view.stop();
+				view.slideDown(100, function() {
+					body.scrollTop(searchBox.offset().top);
+				})
 
-		}
+			}
+		});
+	}
+
 });
-}
-
-});
-
-
